7a7c546ce248254ecf9e51fbd4efeebec1397a78
Mike K T Tabi <42524675+Kwabs7991@users.noreply.github.com>
2019-10-23 23:35:32 +0100

    Create HelloWorld_HolaMundo.java

---
+++ HelloWorld_HolaMundo.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!"); // Print Hello World in English as I can speak English... barely.
+        System.out.println("Hola Mundo!"); // Print Hello World in Spanish as I can speak Spanish
+        System.out.println("My Name is Kobbie Mike Tabi. I am 22."); //Letting you know what an amazing name I have and what my age is.
+    }
+
+    // A code block is used to define a block of code, this is presented via curly brackets
+}

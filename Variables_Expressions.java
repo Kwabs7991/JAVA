57c97769c8effa3484cc999b1a8b017863b9e7f6
Mike K T Tabi <42524675+Kwabs7991@users.noreply.github.com>
2019-10-23 23:40:39 +0100

    Create Variables_Expressions.java

---
+++ Variables_Expressions.java
@@ -0,0 +1,19 @@
+public class Variables_Expressions {
+    public static void main(String[] args) {
+        //declare variables
+
+        int myFirstNumber = (11 * 22) - (6 * 2 + 22);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myfourthNumber = myFirstNumber + mySecondNumber;
+        int end_Result = myFirstNumber - myThirdNumber + mySecondNumber;
+        int endResultTwo = myfourthNumber;
+
+        int myLastValue = end_Result - 1000;
+
+
+        System.out.println(end_Result);
+        System.out.println(endResultTwo);
+        System.out.println(myLastValue);
+    }
+}

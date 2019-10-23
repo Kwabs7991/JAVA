0b173ec50ee78eaeac9449cb9ec25f934c899854
Mike K T Tabi <42524675+Kwabs7991@users.noreply.github.com>
2019-10-23 23:39:06 +0100

    Create Variables_Datatypes.java

---
+++ Variables_Datatypes.java
@@ -0,0 +1,15 @@
+public class Variables_Datatypes {
+
+    public static void main(String[] args) {
+        int myNumeroNumbre = 5; // This is a declaration statement. It is used to define a variable by indicating the data type, name,etc.
+        String myFirstName = "Miguel";
+        int batmanBegins = 1939;
+        int calc1 = 10+4 * (9-2+11); //Java Operators perform an operation on a variable or value.
+
+        System.out.println(myNumeroNumbre);
+        System.out.println(myFirstName);
+        System.out.println("Batman began his crime fighting crusade in the year " + batmanBegins);
+        System.out.println(calc1 + "... quick maths!");
+    }
+
+ }
